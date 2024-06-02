8ed99435a89b601fdb0dbe776c4eff3253544744
FASTCODING Thanh <ngo.thanh@fastcoding.jp>
2024-06-02 16:56:11 +0700

    [upd] api orders

---
+++ src/controllers/common/order_status.controller.js
@@ -0,0 +1,18 @@
+const logger = require('../../utils/logger');
+const db = require('../../models');
+
+exports.index = async (req, res) => {
+  try {
+    const data = await db.order_status.findAll({
+      attributes: ['id', 'name', 'description'],
+    });
+    res.json({
+      data,
+    });
+  } catch (err) {
+    logger.error(err);
+    res.status(500).send({
+      message: err.message || 'Some error occurred',
+    });
+  }
+};
